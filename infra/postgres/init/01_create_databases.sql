fe5758349fe97835230853c129ce12b8dd764264
Heloi Tanoue <h231761@dac.unicamp.br>
2026-04-30 23:42:34 -0300

    Create initial MVP infrastructure for Projeto Garcia

---
+++ infra/postgres/init/01_create_databases.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE n8n_db OWNER garcia_admin;
+CREATE DATABASE keycloak_db OWNER garcia_admin;
+CREATE DATABASE openproject_db OWNER garcia_admin;
+CREATE DATABASE superset_db OWNER garcia_admin;
